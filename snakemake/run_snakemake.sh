6dbcac981797c3cd40ab14fd907205fd008faba4
Sebastian Kurscheid <genomicsio@gmail.com>
2017-05-03 15:36:44 +1000

    implements all required changes for current stage of workflow being fully functional

--- snakemake/run_snakemake.sh
+++ snakemake/run_snakemake.sh
@@ -2,24 +2,27 @@
 
 export cluster_config="cluster.yaml"
 
+snakemake --dag | dot -Tpdf > dag.pdf
+snakemake --rulegraph | dot -Tpdf > rulegraph.pdf
+
 if uname -a | grep "raijin"
 	then
 		export PATH="/short/rl2/miniconda3/envs/snakemake/bin/":$PATH;
 		export cluster_config="raijin.yaml"
+		snakemake -p --configfile config.yaml \
+					 	  --snakefile Snakefile \
+						  --jobs 10 \
+						  --printshellcmds \
+						  --cluster "qsub -pe threads {threads} \
+					            				-q {cluster.queue} \
+					                    -l virtual_free={cluster.virtual_free} \
+					                    -l h_vmem={cluster.h_vmem} \
+					                    -o {cluster.outstream} \
+					                    -e {cluster.errorstream}" \
+					    --cluster-config $cluster_config
+	else
+		snakemake -p --configfile config.yaml \
+					 	  --snakefile Snakefile \
+						  --jobs 10 \
+						  --printshellcmds
 fi
-
-snakemake --dag | dot -Tpdf > dag.pdf
-snakemake --rulegraph | dot -Tpdf > rulegraph.pdf
-
-snakemake -p --configfile config.yaml \
- 	  --snakefile Snakefile \
-	  --jobs 10 \
-	  --printshellcmds \
-	  --cluster "qsub -pe threads {threads} \
-                    	-q {cluster.queue} \
-                       	-l virtual_free={cluster.virtual_free} \
-                       	-l h_vmem={cluster.h_vmem} \
-                       	-o {cluster.outstream} \
-                       	-e {cluster.errorstream}" \
-          --cluster-config $cluster_config
-	  
